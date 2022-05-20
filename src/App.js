c2ef70229f816fbc1064aab0ca212431b60686e4
Jared Poetter <poet0007@umn.edu>
2022-05-20 11:15:38 -0500

    Switching the App component to be a function

--- src/App.js
+++ src/App.js
@@ -9,43 +9,28 @@ import Stops from './components/stops';
 import Information from './components/information';
 import NotFound from './components/not-found';
 
-export default class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            routes: [],
-            directions: [],
-            stops: [],
-            stopInformation: {},
-            selectedRoute: '',
-            selectedDirection: '',
-            selectedStop: '',
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>My NextTrip App</h1>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<TransitRoutes />}></Route>
-                        <Route
-                            path="/route/:routeId"
-                            element={<Directions />}
-                        ></Route>
-                        <Route
-                            path="/route/:routeId/direction/:directionId"
-                            element={<Stops />}
-                        ></Route>
-                        <Route
-                            path="/route/:routeId/direction/:directionId/stop/:stopId"
-                            element={<Information />}
-                        ></Route>
-                        <Route path="*" element={<NotFound />}></Route>
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div>
+            <h1>My NextTrip App</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<TransitRoutes />}></Route>
+                    <Route
+                        path="/route/:routeId"
+                        element={<Directions />}
+                    ></Route>
+                    <Route
+                        path="/route/:routeId/direction/:directionId"
+                        element={<Stops />}
+                    ></Route>
+                    <Route
+                        path="/route/:routeId/direction/:directionId/stop/:stopId"
+                        element={<Information />}
+                    ></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
